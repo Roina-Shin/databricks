a4b4cec5d62c4bc645df11ae1ecdf96dcd49f785
Yejin Shin <roinashin@gmail.com>
2024-10-20 21:16:24 +0900

    Add challenge

--- sql_challenge.sql
+++ sql_challenge.sql
@@ -23,3 +23,25 @@ Short	517224
 Medium	251347
 Long	18412
 */
+
+
+/*
+I'd like you to return a query that shows the most popular combination of start and end stations
+in terms of the total number of journeys across the dataset.
+
+The start and end stations should be in the format:
+'start_station_name TO end_station_name'
+
+Use the jc_bike_data_22 and station_data table.
+*/
+
+
+select concat(start_station, ' TO ', end_station) as most_popular_journey, journey_counts
+from (
+select jbd.start_station_id, std.station_name as start_station, jbd.end_station_id, std2.station_name as end_station, count(*) as journey_counts
+from jc_bike_data_22 jbd
+join station_data std on jbd.start_station_id = std.station_id
+join station_data std2 on jbd.end_station_id = std2.station_id
+group by 1, 2, 3, 4
+order by 5 desc
+limit 1)
